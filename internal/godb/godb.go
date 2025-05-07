14290d673df691fdc830063960957df560e3008b
ilexsor <ilexsor@gmail.com>
2025-05-07 22:41:49 +0400

    add errorIs check

--- internal/godb/godb.go
+++ internal/godb/godb.go
@@ -3,33 +3,35 @@ package godb
 import (
 	"context"
 	"crypto/rand"
+	"errors"
 	"fmt"
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/pgxpool"
 	"godb/pkg/helpers/wordz"
 	"math/big"
 	"time"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Instance struct {
 	Db *pgxpool.Pool
 }
 
-func (i *Instance) Start()  {
+func (i *Instance) Start() {
 	fmt.Println("Project godb started!")
 	//i.initUsers()
 
 }
 
-//Структура пользователя
+// Структура пользователя
 type User struct {
-	Id int
-	Name string
-	Age int
+	Id       int
+	Name     string
+	Age      int
 	IsVerify bool
 }
 
-func (i *Instance) initUsers (){
+func (i *Instance) initUsers() {
 	wordz.Prefix = ""
 	for k := 0; k < 100; k++ {
 		user := &User{}
@@ -41,18 +43,18 @@ func (i *Instance) initUsers (){
 		i.addUser(context.Background(), user)
 	}
 
-	users, _ :=i.getVerifyUsers(context.Background())
+	users, _ := i.getVerifyUsers(context.Background())
 
 	for _, u := range users {
 		wordz.Words = []string{"Turgenev", "Lebedev", "Ivanov", "Smirnov", "Preobrazhensky", "Yahontov", "Chernyshevsky", "Kuznetsov"}
 		lastName := wordz.Random()
-		i.updateNameById(context.Background(), u.Id, u.Name + " " +  lastName)
+		i.updateNameById(context.Background(), u.Id, u.Name+" "+lastName)
 	}
 
 	i.removeUnverified(context.Background())
 }
 
-func (i *Instance) updateUserAge(ctx context.Context, name string, age int)  {
+func (i *Instance) updateUserAge(ctx context.Context, name string, age int) {
 	_, err := i.Db.Exec(ctx, "UPDATE users SET age=$1 WHERE name=$2;", age, name)
 	if err != nil {
 		fmt.Println(err)
@@ -60,7 +62,7 @@ func (i *Instance) updateUserAge(ctx context.Context, name string, age int)  {
 	}
 }
 
-func (i *Instance) getUserByName(ctx context.Context, name string)  {
+func (i *Instance) getUserByName(ctx context.Context, name string) {
 	//Выполнение самого запроса. И получение структуры rows, которая содержит в себе строки из базы данных.
 	user := &User{}
 	err := i.Db.QueryRow(ctx, "SELECT name, age, verify FROM users WHERE name=$1 LIMIT 1;", name).Scan(&user.Name, &user.Age, &user.IsVerify)
@@ -71,22 +73,22 @@ func (i *Instance) getUserByName(ctx context.Context, name string)  {
 	fmt.Printf("User by name: %v\n", user)
 }
 
-//Функция, которая получает список пользователей
+// Функция, которая получает список пользователей
 func (i *Instance) getAllUsers(ctx context.Context) {
 	//Определяем слайс users, куда будем складывать всех пользователей, которых получим из базы
 	var users []User
 
 	//Выполнение самого запроса. И получение структуры rows, которая содержит в себе строки из базы данных.
 	rows, err := i.Db.Query(ctx, "SELECT name, age, verify FROM users;")
-	if err == pgx.ErrNoRows {
-		fmt.Println("No rows")
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		fmt.Println("Users not found")
 		return
 	} else if err != nil {
 		fmt.Println(err)
 	}
 	//После того как все действия со строками будут выполнены, обязательно и всегда нужно закрывать структуру rows. Для избежания утечек памяти и утечек конектов к базе
 	defer rows.Close()
-
 
 	for rows.Next() {
 		user := User{}
@@ -97,14 +99,14 @@ func (i *Instance) getAllUsers(ctx context.Context) {
 	fmt.Println(users)
 }
 
-func (i *Instance) removeUnverified(ctx context.Context)  {
+func (i *Instance) removeUnverified(ctx context.Context) {
 	_, err := i.Db.Exec(ctx, "DELETE FROM users WHERE verify=false")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 }
-func (i *Instance) updateNameById(ctx context.Context, id int, name string)  {
+func (i *Instance) updateNameById(ctx context.Context, id int, name string) {
 	_, err := i.Db.Exec(ctx, "UPDATE users SET name=$1 WHERE id=$2;", name, id)
 	if err != nil {
 		fmt.Println(err)
@@ -125,7 +127,6 @@ func (i *Instance) getVerifyUsers(ctx context.Context) ([]User, error) {
 	//После того как все действия со строками будут выполнены, обязательно и всегда нужно закрывать структуру rows. Для избежания утечек памяти и утечек конектов к базе
 	defer rows.Close()
 
-
 	for rows.Next() {
 		user := User{}
 		rows.Scan(&user.Id, &user.Name, &user.Age, &user.IsVerify)
@@ -145,4 +146,3 @@ func (i *Instance) addUser(ctx context.Context, user *User) {
 	fmt.Println(commandTag.String())
 	fmt.Println(commandTag.RowsAffected())
 }
-
